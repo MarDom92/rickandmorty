45fd94bad37121af9f63f2af3ce532e6bb421f8f
MarDom92 <marcin.domanski92@gmail.com>
2022-04-23 22:27:31 +0200

    remove Result class - duplicate of Episode class

--- src/main/java/pl/envelo/rickandmorty/model/Response.java
+++ src/main/java/pl/envelo/rickandmorty/model/Response.java
@@ -1,6 +1,7 @@
 package pl.envelo.rickandmorty.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +18,7 @@ import java.util.List;
 public class Response {
 
     private Info info;
-    private List<Result> results = null;
+    @JsonProperty("results")
+    private List<Episode> episodes = null;
 
 }
